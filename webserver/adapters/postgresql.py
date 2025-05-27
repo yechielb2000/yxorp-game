4c12858c925eca3363da6945b84e52650d6a80dd
Yechiel Babani <yechielb2000@gmail.com>
2025-05-27 16:03:26 +0300

    get postgres url from settings

--- webserver/adapters/postgresql.py
+++ webserver/adapters/postgresql.py
@@ -1,18 +1,10 @@
-import os
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-USER = os.environ.get("POSTGRESQL_USER")
-PASSWORD = os.environ.get("POSTGRESQL_PASSWORD")
-DATABASE = os.environ.get("POSTGRESQL_DB")
-HOST = os.environ.get("POSTGRESQL_HOST")
-PORT = os.environ.get("POSTGRESQL_PORT")
-
-DATABASE_URL = f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+from webserver.settings import Settings
 
 BasePG = declarative_base()
-pg_engine = create_async_engine(DATABASE_URL, echo=True)
+pg_engine = create_async_engine(Settings.postgresql_url, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(bind=pg_engine, expire_on_commit=False, class_=AsyncSession)
 
