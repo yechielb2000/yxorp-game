cfb8dded1eb45c956c6796b3005decb15004fd35
Yechiel Babani <yechielb2000@gmail.com>
2025-05-28 20:13:38 +0300

    ok i did it wrong last time. using BaseHTTPMiddleware now

--- proxyserver/proxyserver/middlewares/rate_limit.py
+++ proxyserver/proxyserver/middlewares/rate_limit.py
@@ -2,19 +2,25 @@ from http import HTTPStatus
 
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 
-MAX_REQUESTS_PER_SECOND = 20
 
+class RedisMiddleware(BaseHTTPMiddleware):
+    """
+    For each ip update the redis counter.
+    If in exceed the request limit of `MAX_REQUESTS_PER_SECOND`, return 429.
+    """
+    MAX_REQUESTS_PER_SECOND = 20
 
-async def rate_limiter_middleware(request: Request, call_next):
-    ip = request.client.host
-    key = f"ratelimit:{ip}"
+    async def dispatch(self, request: Request, call_next):
+        ip = request.client.host
+        key = f"ratelimit:{ip}"
 
-    count = await request.state.redis_client.incr(key)
-    if count == 1:
-        await request.state.redis_client.expire(key, 1)
+        count = await request.state.redis_client.incr(key)
+        if count == 1:
+            await request.state.redis_client.expire(key, 1)
 
-    if count > MAX_REQUESTS_PER_SECOND:
-        return JSONResponse(status_code=HTTPStatus.TOO_MANY_REQUESTS, content={"error": "Too many requests"})
+        if count > self.MAX_REQUESTS_PER_SECOND:
+            return JSONResponse(status_code=HTTPStatus.TOO_MANY_REQUESTS, content={"error": "Too many requests"})
 
-    return await call_next(request)
+        return await call_next(request)
