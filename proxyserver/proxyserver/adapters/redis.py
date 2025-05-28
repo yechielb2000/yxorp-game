15e2bbf329355016c56642fd9223e5b051a3e18e
Yechiel Babani <yechielb2000@gmail.com>
2025-05-28 19:50:59 +0300

    use regular Redis for now

--- proxyserver/proxyserver/adapters/redis.py
+++ proxyserver/proxyserver/adapters/redis.py
@@ -1,17 +1,12 @@
-import redis.asyncio as redis
+from redis.asyncio import Redis
+
 from settings import settings
 
-import redis
-
-def create_redis():
-  return redis.ConnectionPool(
-    host=settings.redis_host,
-    port=settings.redis_port,
-    db=0,
-    decode_responses=True
-  )
-
-redis_instance = create_redis()
 
 def get_redis():
-  return redis.Redis(connection_pool=redis_instance)
+    return Redis(
+        host=settings.redis_host,
+        port=settings.redis_port,
+        db=0,
+        decode_responses=True
+    )
--- proxyserver/proxyserver/app.py
+++ proxyserver/proxyserver/app.py
@@ -1,8 +1,17 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from settings import settings
 import uvicorn
-
+from proxyserver.adapters.redis import get_redis
 app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(a: FastAPI):
+    redis_client = get_redis()
+
+    yield
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host="0.0.0.0", port=settings.webserver_port) # TODO: should be as webserver port.
