4868d510ef99ce4d64cbca48f2594defdf3e5813
Yechiel Babani <yechielb2000@gmail.com>
2025-05-28 17:14:31 +0300

    add basic app

---
+++ proxyserver/proxyserver/app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host="0.0.0.0", port=8005) # TODO: should be as webserver port.
