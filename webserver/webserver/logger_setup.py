a66749bb3ddc7250b5a0910834063e3ce4294931
Yechiel Babani <yechielb2000@gmail.com>
2025-05-29 20:19:28 +0300

    split loggers

--- webserver/webserver/logger_setup.py
+++ webserver/webserver/logger_setup.py
@@ -5,6 +5,9 @@ from loguru import logger
 
 from webserver.adapters.elastic import get_elasticsearch_client
 
+_infra_logger = None
+_user_logger = None
+
 
 class ElasticSearchLogSink:
     def __init__(self, elastic: Elasticsearch):
@@ -40,8 +43,24 @@ class ElasticSearchLogSink:
 
 
 def setup_logger():
+    global _infra_logger, _user_logger
+
     elastic_client = get_elasticsearch_client()
     es_sink = ElasticSearchLogSink(elastic_client)
 
     logger.remove()
     logger.add(es_sink, level=logging.INFO)
+    _infra_logger = logger.bind(log_type="infra")
+    _user_logger = logger.bind(log_type="user-actions")
+
+
+def get_infra_logger():
+    if not _infra_logger:
+        raise ValueError("Infra logger is not initialized yet.")
+    return _infra_logger
+
+
+def get_user_logger():
+    if not _user_logger:
+        raise ValueError("User logger is not initialized yet.")
+    return _user_logger
