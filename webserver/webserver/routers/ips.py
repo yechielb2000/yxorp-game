b3f9ef83f3c0b68ab257b885e6182b88799f0a1d
Yechiel Babani <yechielb2000@gmail.com>
2025-05-29 20:30:34 +0300

    log out logs in ip route

--- webserver/webserver/routers/ips.py
+++ webserver/webserver/routers/ips.py
@@ -2,10 +2,10 @@ from http import HTTPStatus
 
 import ipwhois
 from fastapi import APIRouter, Depends
-from loguru import logger
 from pydantic import IPvAnyAddress
 from starlette.responses import JSONResponse
 
+from logger_setup import get_user_logger, get_infra_logger
 from webserver.controllers.ips import IpsController, get_ips_controller
 
 ips_router = APIRouter(
@@ -17,11 +17,13 @@ ips_router = APIRouter(
 @ips_router.get('/{ip}/country')
 async def get_ip_info(ip: IPvAnyAddress, ips_controller: IpsController = Depends(get_ips_controller)):
     ip = str(ip)
-    logger.info("Request query for ip started", extra={'ip_address': ip})
+    get_user_logger().info("Request query for ip started", extra={'ip_address': ip})
     whois = ipwhois.IPWhois(ip)
+    get_infra_logger().info("Looking up ip info for ip: ", extra={'ip_address': ip})
     results = whois.lookup_rdap()
     country = results.get('network', {}).get('country')
     if country:
+        get_infra_logger().info("Found country for ip: ", extra={'ip_address': ip})
         return await ips_controller.upsert_ip_info(ip, country)
-    logger.debug("IP info not found", extra={'ip_address': ip})
+    get_infra_logger().info("IP info not found", extra={'ip_address': ip})
     return JSONResponse(status_code=HTTPStatus.NO_CONTENT, content={"error": "IP info not found"})
