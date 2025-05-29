110da3ff252cc4ced423554039ec4d548fd2dc24
Yechiel Babani <yechielb2000@gmail.com>
2025-05-29 20:27:09 +0300

    rename logger

--- webserver/webserver/controllers/ips.py
+++ webserver/webserver/controllers/ips.py
@@ -15,10 +15,10 @@ from webserver.schemas.ip_address import IPAddressCreate, IPAddressUpdate, IPAdd
 class IpsController:
     def __init__(self, postgres: AsyncSession):
         self.postgres = postgres
-        self.logger = get_infra_logger()
+        self.infra_logger = get_infra_logger()
 
     async def get_ip_info(self, address: str) -> Optional[IPAddressRead]:
-        self.logger.info("Get ip info", extra={"address": address})
+        self.infra_logger.info("Get ip info", extra={"address": address})
         stmt = select(IPAddress).where(IPAddress.address == address)
         result = await self.postgres.execute(stmt)
         row = result.scalar_one_or_none()
@@ -27,13 +27,13 @@ class IpsController:
         return None
 
     async def create_ip_info(self, ip_info: IPAddressCreate) -> None:
-        self.logger.info("Create ip info", extra={"ip_info": ip_info.model_dump()})
+        self.infra_logger.info("Create ip info", extra={"ip_info": ip_info.model_dump()})
         stmt = insert(IPAddress).values(ip_info.model_dump())
         await self.postgres.execute(stmt)
         await self.postgres.commit()
 
     async def upsert_ip_info(self, ip: str, country: str) -> IPAddressRead:
-        self.logger.info("Upsert ip info", extra={"ip": ip, "country": country})
+        self.infra_logger.info("Upsert ip info", extra={"ip": ip, "country": country})
         stmt = insert(IPAddress).values(address=ip, country=country)
         stmt = stmt.on_conflict_do_update(
             index_elements=[IPAddress.address],
@@ -46,13 +46,13 @@ class IpsController:
         return IPAddressRead(address=row.address, country=row.country, searched_at=row.searched_at)
 
     async def delete_ip_info(self, address: str) -> None:
-        self.logger.info("Delete ip info", extra={"address": address})
+        self.infra_logger.info("Delete ip info", extra={"address": address})
         stmt = delete(IPAddress).where(IPAddress.address == address)
         await self.postgres.execute(stmt)
         await self.postgres.commit()
 
     async def update_ip_info(self, ip_info: IPAddressUpdate) -> None:
-        self.logger.info("Update ip info", extra={"ip_info": ip_info.model_dump()})
+        self.infra_logger.info("Update ip info", extra={"ip_info": ip_info.model_dump()})
         address = ip_info.address
         update_values = ip_info.model_dump(exclude_none=True, exclude={"address"})
         stmt = update(IPAddress).where(IPAddress.address == address).values(**update_values)
@@ -60,7 +60,7 @@ class IpsController:
         await self.postgres.commit()
 
     async def get_top_queried_countries(self, top: int = 5) -> List[str]:
-        self.logger.info("Get top queried countries", extra={"top": top})
+        self.infra_logger.info("Get top queried countries", extra={"top": top})
         stmt = (
             select(IPAddress.country)
             .group_by(IPAddress.country)
@@ -76,8 +76,8 @@ class IpsController:
             country: str,
             start_time: Optional[datetime],
             end_time: Optional[datetime]) -> List[str]:
-        self.logger.info("Get ips by country",
-                         extra={"country": country, "start_time": start_time, "end_time": end_time})
+        self.infra_logger.info("Get ips by country",
+                               extra={"country": country, "start_time": start_time, "end_time": end_time})
         stmt = select(IPAddress.address).where(IPAddress.country == country)
 
         if start_time and end_time:
