ff5cbcb0ab70a44f22a995f5d4318b1793e7abb7
Yechiel Babani <yechielb2000@gmail.com>
2025-05-31 21:22:34 +0300

    use geoip insttead of whois

--- webserver/webserver/routers/ips.py
+++ webserver/webserver/routers/ips.py
@@ -1,11 +1,12 @@
-import ipaddress
 from http import HTTPStatus
 
 import ipwhois
 from fastapi import APIRouter, Depends
+from geoip2fast import GeoIP2Fast
 from pydantic import IPvAnyAddress
 from starlette.responses import JSONResponse
 
+from webserver.adapters.geoip import get_geoip
 from webserver.controllers.ips import IpsController, get_ips_controller
 from webserver.utils.jwt_token import get_current_user
 from webserver.utils.logger_setup import get_user_logger, get_infra_logger
@@ -17,26 +18,24 @@ ips_router = APIRouter(
 
 
 @ips_router.get('/{ip}/country')
-async def get_ip_info(ip: IPvAnyAddress, ips_controller: IpsController = Depends(get_ips_controller)):
-    # TODO: fix not an ip bug 8.8.4.4
+async def get_ip_info(
+        ip: IPvAnyAddress,
+        ips_controller: IpsController = Depends(get_ips_controller),
+        geo_ip: GeoIP2Fast = Depends(get_geoip)
+):
     get_user_logger().info("Request query for ip started", extra={'ip_address': ip})
     ip = str(ip)
 
-    try:
-        ip_obj = ipaddress.ip_address(ip)
-        if ip_obj.is_global:
-            get_infra_logger().info("Looking up ip info for ip: ", extra={'ip_address': ip})
-            whois = ipwhois.IPWhois(ip)
-            search_result = whois.lookup_rdap(get_asn_description=False)
-            country = search_result.get('network', {}).get('country')
-            if country:
-                get_infra_logger().info("Found country for ip: ", extra={'ip_address': ip})
-                return await ips_controller.upsert_ip_info(ip, country)
-        else:
-            get_infra_logger().info("IP is not global", extra={'ip_address': ip})
-            raise ipwhois.IPDefinedError("IP is not global")
-    except ipwhois.IPDefinedError as e:
-        get_infra_logger().error("IP defined error", extra={'ip_address': ip}, exc_info=e)
+    ip_info = geo_ip.lookup(ip)
 
-    get_infra_logger().info("IP country not found", extra={'ip_address': ip})
-    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content="Could not find country for ip")
+    get_infra_logger().info("Checking if IP is private", extra={'ip_address': ip})
+
+    if ip_info.is_private:
+        return JSONResponse(status_code=HTTPStatus.BAD_REQUEST, content="IP is private")
+
+    get_infra_logger().info("Looking up ip country", extra={'ip_address': ip})
+    if not ip_info.country_code:
+        return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content="Could not find country for ip")
+
+    get_infra_logger().info("Found country for ip: ", extra={'ip_address': ip})
+    return await ips_controller.upsert_ip_info(ip, ip_info.country_code)
